0538a5edb56e6082cbd378e99b2df718a0e8504a
LynnAmsbury <Lynn8542@gmail.com>
2020-06-01 15:33:09 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,31 +1,62 @@
-class Show < ActiveRecord::Base
+# class Show < ActiveRecord::Base
 
+#     def self.highest_rating
+#         self.maximum(:rating)
+#     end
+
+#     def self.most_popular_show
+#         self.where("rating = ?", self.highest_rating).first
+#     end
+
+#     def self.lowest_rating
+#         self.minimum(:rating)
+#     end
+
+#     def self.least_popular_show
+#         self.where("rating = ?", self.lowest_rating).first
+#     end
+
+#     def self.ratings_sum
+#         self.sum(:rating)
+#     end
+
+#     def self.popular_shows
+#         self.where("rating > ?", 5)
+#     end
+
+#     def self.shows_by_alphabetical_order
+#         self.order(:name, :asc)
+#     end
+
+# end
+
+class Show < ActiveRecord::Base
     def self.highest_rating
-        self.maximum(:rating)
+        maximum("rating")
     end
 
     def self.most_popular_show
-        self.where("rating = ?", self.highest_rating).first
+        order("rating DESC").first
     end
 
     def self.lowest_rating
-        self.minimum(:rating)
+        minimum("rating")
     end
 
     def self.least_popular_show
-        self.where("rating = ?", self.lowest_rating).first
+        order("rating ASC").first
     end
 
     def self.ratings_sum
-        self.sum(:rating)
+        sum("rating")
     end
 
     def self.popular_shows
-        self.where("rating > ?", 5)
+        where("rating > 5")
     end
 
     def self.shows_by_alphabetical_order
-        self.order(:name, :asc)
+        order("name ASC")
     end
 
 end
